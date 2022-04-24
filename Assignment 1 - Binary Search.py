c2f19d53cf34c3c3484f1de5ae5738e6d6eaad7a
the-dark-eye <nilesh.kala@konverge.ai>
2022-04-24 12:52:22 +0530

    Added commentary

--- Assignment 1 - Binary Search.py
+++ Assignment 1 - Binary Search.py
@@ -14,10 +14,10 @@ Outputs: Number by which list has been rotated
 """
 
 def count_rotations_linear(nums):
-    """_summary_
+    """Linear search function that counts how many times a list needs to be rotated to be in ascending order
 
     Args:
-        nums (_type_): _description_
+        nums (list): list of numbers in ascending order, rotated by some number
     """
     
     position = 0                 # Initialize position variable
@@ -33,31 +33,59 @@ def count_rotations_linear(nums):
     
     return 0                     # return 0 if none of the positions passed the check
 
+# Set up test cases
+
+test1 = [27, 29, 3, 5, 6, 7, 9, 11, 14]
+test2 = [0, 1]
+test3 = []
+test4 = [10, -1, 3, 5]
+test5 = [1]
+
+# Check brute force solution for test cases
+
+print("Checking linear search algorithm")
+print(count_rotations_linear(test1))
+print(count_rotations_linear(test2))
+print(count_rotations_linear(test3))
+print(count_rotations_linear(test4))
+print(count_rotations_linear(test5))
+
+# Optimize algorithm using binary search
+
 def count_rotations_binary(nums):
-    """
+    """Binary search function to count how many times a list needs to be rotated for it 
+    to be in ascending order
 
     Args:
-        nums (_type_): _description_
+        nums (list): List of integers, rotated by some number
     """
     
     N = len(nums)
     low = 0
     high = N
     
-    if N > 1:
+    if N > 1:   # If length of list is more than 1
             
-        while low < high:
+        while low < high:   # Terminating condition
             
             mid = (low + high)//2
             
-            if nums[mid] < nums[mid - 1]:
+            if nums[mid] < nums[mid - 1]:   # Check if number at mid position is greater
+                                            # than the number preceeding it
                 return mid
-            elif nums[mid] > nums[-1]:
+            elif nums[mid] > nums[-1]:  # Check if number at mid is greater than number at
+                                        # end of the list
                 low = mid + 1
             else:
-                high = mid - 1
+                high = mid  # else change the length of search list to first half
 
-    return 0
+    return 0    # Return 0 if lenght of list is 1 or less than 1
 
-# print(count_rotations_linear([27, 29, 3, 5, 6, 7, 9, 11, 14]))
-print(count_rotations_binary([3, 5, 6, 7, 9, 11, 14]))
+# Check binary solution for test cases
+
+print("Checking binary search algorithm")
+print(count_rotations_binary(test1))
+print(count_rotations_binary(test2))
+print(count_rotations_binary(test3))
+print(count_rotations_binary(test4))
+print(count_rotations_binary(test5))
